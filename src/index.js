bb649659bd67a92114410a2e8c4e5456b1f849cf
roman01la <roman01la@romanliutikov.com>
2023-04-01 16:08:07 +0300

    wip

---
+++ src/index.js
@@ -0,0 +1,80 @@
+const fs = require("fs");
+const path = require("path");
+const { exec } = require("child_process");
+const axios = require("axios");
+const pack = require("tar-pack");
+const { program } = require("commander");
+const prettier = require("prettier");
+const pkg = require("../package.json");
+
+program
+  .name(pkg.name)
+  .description(pkg.description)
+  .version(pkg.version)
+  .argument("<project-name>", "directory where a project will be created");
+
+program.parse();
+
+const [projectName] = program.args;
+
+if (!projectName) {
+  program.help();
+} else {
+  console.log(
+    "Downloading project template from https://github.com/pitch-io/uix-starter..."
+  );
+  axios
+    .get("https://github.com/pitch-io/uix-starter/archive/master.tar.gz", {
+      responseType: "stream",
+    })
+    .then(
+      (r) =>
+        new Promise((resolve, reject) => {
+          console.log(`Unpacking into ${projectName}...`);
+          r.data.pipe(
+            pack.unpack(projectName, (err) => {
+              if (err) {
+                reject(err);
+              } else {
+                resolve();
+              }
+            })
+          );
+        })
+    )
+    .then(() => {
+      const pkgjson = JSON.parse(
+        fs.readFileSync(path.join(projectName, "package.json"), "utf8")
+      );
+      pkgjson.name = projectName;
+      fs.writeFileSync(
+        path.join(projectName, "package.json"),
+        prettier.format(JSON.stringify(pkgjson), {
+          parser: "json",
+        })
+      );
+      const readme = fs.readFileSync(
+        path.join(projectName, "README.md"),
+        "utf8"
+      );
+      fs.writeFileSync(
+        path.join(projectName, "README.md"),
+        readme
+          .replace("uix-starter", projectName)
+          .split("\n")
+          .filter((l) => !l.startsWith("Template project"))
+          .join("\n")
+      );
+      console.log("Installing dependencies...");
+      exec(`cd ${projectName} && yarn install`, (err) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log("Done.");
+          console.log("\n");
+          console.log("yarn dev # run dev build in watch mode with CLJS REPL");
+          console.log("yarn release # build production bundle");
+        }
+      });
+    });
+}
